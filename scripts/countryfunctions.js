c8a069e1863f129a6aea21f85a904f124014fc14
TroyMcCulloch <troym.19040@waitarahs.school.nz>
2023-09-04 14:00:48 +1200

    04/09/23

---
+++ scripts/countryfunctions.js
@@ -0,0 +1,35 @@
+function NZFunction() { 
+	var popup = document.getElementById("NZ"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt'); 
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function AUFunction() { 
+	var popup = document.getElementById("Aust"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function PapuaFunction() { 
+	var popup = document.getElementById("PapuaNew"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function SolomonFunction() { 
+	var popup = document.getElementById("Solomon"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function VanuatuFunction() { 
+	var popup = document.getElementById("Vanuatu"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function FijiFunction() { 
+	var popup = document.getElementById("Fiji"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
+function TOngaFunction() { 
+	var popup = document.getElementById("Tonga"); popup.classList.toggle("show");
+	$(document.getElementById("worldmapbody")).toggleClass('alt');
+	$(document.getElementById("worldmapimg")).toggleClass('alt');
+}
